fdc99791f013e547b64c35ff713ec7b56f204bb2
Waldemar Figueroa Medina <waldemar@nobox.com>
2015-06-01 13:02:02 -0400

    Add Markdown parser to generate an html README

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,19 @@
+var gulp = require('gulp');
 var elixir = require('laravel-elixir');
+var markdown = require('gulp-markdown');
 require('laravel-elixir-browserify').init('bundler');
+
+elixir.extend('markdown', function() {
+
+    gulp.task('markdown', function() {
+        return gulp.src('README.md')
+            .pipe(markdown())
+            .pipe(gulp.dest('dist'));
+    });
+
+    return this.queueTask('markdown');
+
+});
 
 elixir(function(mix) {
     mix
@@ -10,5 +24,7 @@ elixir(function(mix) {
         .bundler('index.js', {
             rename: 'copaair-dest-select.js'
         })
+
+        .markdown()
     ;
 });
